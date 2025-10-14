3c4504052948dcd702c8fd99fcc0fbdc5b2c73db
Massika-Ke <wamwoka99@gmail.com>
2025-10-14 20:09:05 +0300

    twilio notification-config

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -33,6 +33,11 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function routeNotificationForTwilio()
+    {
+        return $this->phone;
+    }
+
     /**
      * Get the attributes that should be cast.
      *
