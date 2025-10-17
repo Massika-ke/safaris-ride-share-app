fac31b7746e6f473e5d0deff17e1f88bc91780cc
Massika-Ke <wamwoka99@gmail.com>
2025-10-18 01:11:40 +0300

    event-listener to frontend

--- backend/app/Http/Controllers/TripController.php
+++ backend/app/Http/Controllers/TripController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\TripAccepted;
+use App\Events\TripEnded;
+use App\Events\TripLocationUpdated;
+use App\Events\TripStarted;
 use App\Models\Trip;
 use Illuminate\Http\Request;
 
@@ -53,6 +57,8 @@ class TripController extends Controller
 
         $trip->load('driver.user');
 
+        TripAccepted::dispatch($trip, $request->user());
+
         return $trip;
     }
 
@@ -64,6 +70,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripStarted::dispatch($trip, $request->user());
 
         return $trip;
     }
@@ -77,6 +85,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripEnded::dispatch($trip, $request->user());
 
         return $trip;
     }
@@ -94,6 +104,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripLocationUpdated::dispatch($trip, $request->user());
 
         return $trip;
     }
